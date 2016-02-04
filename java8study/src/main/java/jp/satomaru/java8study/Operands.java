f08eb4b31a516125e7261274891cb23b8d65ab5b
sunotora <sunotora@gmail.com>
2016-02-05 02:23:43 +0900

    ２種類のメソッド参照とラムダ式と説明の追加

--- java8study/src/main/java/jp/satomaru/java8study/Operands.java
+++ java8study/src/main/java/jp/satomaru/java8study/Operands.java
@@ -25,10 +25,35 @@ public class Operands {
 	}
 
 	public static void main(String[] args) {
+
 		Operands me = new Operands(BigDecimal.valueOf(1), BigDecimal.valueOf(2));
+
 		// 足し算をさせてみてください。
+
+		// メソッド参照 (Class::staticMethod）
 		System.out.println(me.execute(Operands::add));
-		// R apply(T t, U u);
+
+		// メソッド参照（Class::instanceMethod）
+		System.out.println(me.execute(BigDecimal::add));
+
+		// ラムダ式
+		System.out.println(me.execute((left, right) -> left.add(right)));
+
+		// 関数インターフェース
+		// BinaryOperator<T> extends BiFunction<T,T,T>
+		// T apply(T t, T u)         R apply(T t, U u);
+		// ラムダ式
+		// (T, T) -> T
+		// BinaryOperator<BigDecimal>
+		// BigDecimal apply(BigDecimal t, BigDecimal u)
+		// この関数インターフェースを用いる時、２引数を入力し、同じ型を返す
+		// ラムダ式・関数インターフェース・メソッド参照が使用可能となる
+		// BigDecimal::addは (x, y) -> x.add(y)と同様の処理が実施される
+
+		// object::instanceMethod = (x, y, z) -> instanceMethod(x, y, z)     ex) System.out::println =  x     -> System.out.println(x);
+		// Class::staticMethod    = (x, y, z) -> Class.staticMethod(x, y, z) ex) Math::pow           = (x, y) -> Math.pow(x, y);
+		// Class::instanceMethod  = (x, y, z) -> x.instanceMethod(y, z)      ex) BigDecimal::add     = (x, y) -> x.add(y);
+		// 三番目だけ動作が違うため注意する。
 	}
 
 	private static BigDecimal add(BigDecimal left, BigDecimal right) {
