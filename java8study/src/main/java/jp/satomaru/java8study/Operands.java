e638f6ece1433d96544722288c40b4d762f2877d
Satomaru <erynenadan@gmail.com>
2016-02-05 00:18:49 +0900

    メソッド参照のお題、Operandsを追加。

---
+++ java8study/src/main/java/jp/satomaru/java8study/Operands.java
@@ -0,0 +1,26 @@
+package jp.satomaru.java8study;
+
+import java.math.BigDecimal;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Operands {
+
+	private final BigDecimal left;
+	private final BigDecimal right;
+
+	public BigDecimal execute(BinaryOperator<BigDecimal> action) {
+		return action.apply(left, right);
+	}
+
+	public BigDecimal executeLeft(UnaryOperator<BigDecimal> action) {
+		return action.apply(left);
+	}
+
+	public BigDecimal executeRight(UnaryOperator<BigDecimal> action) {
+		return action.apply(right);
+	}
+}
