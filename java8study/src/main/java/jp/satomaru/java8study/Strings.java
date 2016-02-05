63874aeb54d52d3825fd7d4fd156f02c9cd6edc8
sunotora <sunotora@gmail.com>
2016-02-06 02:36:34 +0900

    途中だけど眠いのでコミット

--- java8study/src/main/java/jp/satomaru/java8study/Strings.java
+++ java8study/src/main/java/jp/satomaru/java8study/Strings.java
@@ -12,11 +12,14 @@ public final class Strings {
 
 	/**
 	 * 数値に変換します。
-	 * 
+	 *
 	 * @param string 数値を表す文字列
 	 * @return 数値（文字列がnullの場合は0）
 	 */
 	public static BigDecimal toBigDecimal(String string) {
+		// nullを許可するOptional<String>を作る
+		// そのOptionalに対してnew BigDecimal(x)を適用 → 型はOptional<BigDecimal>となる
+		// Optionalの中身がnullでなければそれを、nullの場合は0を返却する
 		return Optional.ofNullable(string)
 				.map(BigDecimal::new)
 				.orElse(BigDecimal.ZERO);
@@ -24,19 +27,28 @@ public final class Strings {
 
 	/**
 	 * 文字を繰り返します。
-	 * 
+	 *
 	 * @param target 対象の文字
 	 * @param number 繰り返し回数
 	 * @return 文字列
 	 */
 	public static String repeat(char target, int count) {
+		// targetのストリームを生成
+		// ストリームからcount分取ってくる
+		// 文字列に変換
+		// 全文字列をくっつけて返却
 		return Stream.generate(() -> target)
 				.limit(count)
 				.map(String::valueOf)
 				.collect(Collectors.joining());
 	}
 
-	// TODO 文字数を数えるメソッドを実装しましょう。ただし、nullの時は0文字とします。
+	public static int count(String string) {
+		// 文字数を数えるメソッドを実装しましょう。ただし、nullの時は0文字とします。
+		return Optional.ofNullable(string)
+				.map(String::length)
+				.orElse(0);
+	}
 
 	// TODO "key:value"という書式の文字列で構成された配列からMapを作成しましょう。ただしnullはスキップします。
 
