16789b5a2ffa93241abcec41b729054051d00b8f
sunotora <sunotora@gmail.com>
2016-02-10 00:22:47 +0900

    メソッド名修正

--- java8study/src/test/java/jp/satomaru/java8study/StringsTest.java
+++ java8study/src/test/java/jp/satomaru/java8study/StringsTest.java
@@ -42,7 +42,7 @@ public class StringsTest {
 	}
 
 	@Test
-	public void testmapToStringList() throws Exception {
+	public void testMapToStringList() throws Exception {
 		List<String> actualList = Strings.mapToStringList(map);
 
 		String[] expected = {"key1:value1", "key2:value2","key3:value3","key4:value4","key5:value5"};
