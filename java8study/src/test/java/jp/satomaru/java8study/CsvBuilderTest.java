b225f012beb2bef14ff648dad1383425f06c9d53
sunotora <sunotora@gmail.com>
2016-02-23 01:43:29 +0900

    CsvBuilderのbuildを実装

--- java8study/src/test/java/jp/satomaru/java8study/CsvBuilderTest.java
+++ java8study/src/test/java/jp/satomaru/java8study/CsvBuilderTest.java
@@ -23,6 +23,7 @@ public class CsvBuilderTest {
 		private final int id;
 		private String name;
 		private LocalDate since;
+		private boolean bool;
 	}
 
 	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
@@ -31,20 +32,20 @@ public class CsvBuilderTest {
 
 	@Before
 	public void setUp() {
-		tested = new CsvBuilder<>(entity -> new Object[] {entity.id, entity.name, entity.since});
+		tested = new CsvBuilder<>(entity -> new Object[] {entity.id, entity.name, entity.since, entity.bool});
 		tested.addFormatter(LocalDate.class, date -> date.format(DateTimeFormatter.ofPattern("\"yyyy-MM-dd\"")));
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void testMyEntityToCsv() throws Exception {
 		String actual = tested.build(Arrays.asList(
-				new MyEntity(1, "foo", LocalDate.of(2015, 12, 1)),
-				new MyEntity(2, "bar", LocalDate.of(2016, 2, 15)),
-				new MyEntity(3, "baz", null)));
+				new MyEntity(1, "foo", LocalDate.of(2015, 12, 1), true),
+				new MyEntity(2, "bar", LocalDate.of(2016, 2, 15), false),
+				new MyEntity(3, "baz", null, false)));
 
 		assertThat(actual, is(
-				"1,\"foo\",\"2015-12-01\"" + LINE_SEPARATOR +
-				"2,\"bar\",\"2016-02-15\"" + LINE_SEPARATOR +
-				"3,\"baz\","));
+				"1,\"foo\",\"2015-12-01\",true" + LINE_SEPARATOR +
+				"2,\"bar\",\"2016-02-15\",false" + LINE_SEPARATOR +
+				"3,\"baz\",,false"));
 	}
 }
