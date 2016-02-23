ad9325bd9378edaf1db1823ba8aa4774f1d8ae05
sunotora <sunotora@gmail.com>
2016-02-24 03:19:21 +0900

    途中経過保存

--- java8study/src/main/java/jp/satomaru/java8study/ListMaps.java
+++ java8study/src/main/java/jp/satomaru/java8study/ListMaps.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * リストを値として持つマップの作成を補助します。
- * 
+ *
  * <h3>お題の注釈</h3>
  * <p>
  * このお題は、設計を読み解く為のお題です。
@@ -25,26 +25,26 @@ public class ListMaps<K, E> {
 
 	/**
 	 * マップの作成を開始します。
-	 * 
+	 *
 	 * <p>
 	 * マップには{@link HashMap}、リストには{@link ArrayList}を用います。
 	 * </p>
-	 * 
+	 *
 	 * @return ListMapsオブジェクト
 	 */
 	public static <K, E> ListMaps<K, E> begin() {
-		// TODO 実装してください。
+		return begin(HashMap::new, ArrayList::new);
 	}
 
 	/**
 	 * マップの作成を開始します。
-	 * 
+	 *
 	 * @param mapGenerator マップを生成する関数（通常は、マップ実装クラスのデフォルトコンストラクタ）
 	 * @param listGenerator リストを生成する関数（通常は、リスト実装クラスのデフォルトコンストラクタ）
 	 * @return ListMapsオブジェクト
 	 */
 	public static <K, E> ListMaps<K, E> begin(Supplier<Map<K, List<E>>> mapGenerator, Supplier<List<E>> listGenerator) {
-		// TODO 実装してください。
+		return new ListMaps<K, E>(mapGenerator, listGenerator);
 	}
 
 	/** リストの作成を補助します。 */
@@ -56,21 +56,27 @@ public class ListMaps<K, E> {
 
 		/**
 		 * リストに値を追加します。
-		 * 
+		 *
 		 * @param values 値
 		 * @return このListsオブジェクト自身
 		 */
 		public Lists add(@SuppressWarnings("unchecked") E... values) {
-			// TODO 実装してください。
+
+			// 要素を追加
+			for (E value : values) {
+				current.add(value);
+			}
+			// 自身を返却
+			return this;
 		}
 
 		/**
 		 * リストの作成を終了します。
-		 * 
+		 *
 		 * @return ListMapsオブジェクト
 		 */
 		public ListMaps<K, E> end() {
-			// TODO 実装してください。
+			return ;
 		}
 	}
 
@@ -82,40 +88,48 @@ public class ListMaps<K, E> {
 
 	/**
 	 * コンストラクタ。
-	 * 
+	 *
 	 * @param mapGenerator マップを生成する関数
 	 * @param listGenerator リストを生成する関数
 	 */
 	private ListMaps(Supplier<Map<K, List<E>>> mapGenerator, Supplier<List<E>> listGenerator) {
-		// TODO 実装してください。
+		instance = mapGenerator.get();
+		this.listGenerator = listGenerator;
 	}
 
 	/**
 	 * リストの作成を開始します。
-	 * 
+	 *
 	 * @param key このリストに割り当てる、マップのキー
 	 * @return リストの作成に用いるListsオブジェクト
 	 */
 	public Lists begin(K key) {
-		// TODO 実装してください。
+
+		if (!instance.containsKey(key)) {
+			// 存在する場合
+			return instance.get(key);
+		} else {
+			// 存在しない場合
+			instance.put(key, new ArrayList<>());
+		}
 	}
 
 	/**
 	 * マップの作成を終了します。
-	 * 
+	 *
 	 * @param immutable マップを不変オブジェクトとして受け取る場合はtrue
 	 * @return 作成したマップ
 	 */
 	public Map<K, List<E>> end(boolean immutable) {
-		// TODO 実装してください。
+		return Collections.unmodifiableMap(instance);
 	}
 
 	/**
 	 * マップの作成を終了します。
-	 * 
+	 *
 	 * @return 作成したマップ（変更可能）
 	 */
 	public Map<K, List<E>> end() {
-		// TODO 実装してください。
+		return instance;
 	}
 }
