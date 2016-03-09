83ca9176b3161779d13df0091f12bcdca95fc386
sunotora <sunotora@gmail.com>
2016-03-10 02:38:19 +0900

    マージするためにコミット

--- java8study.mastermind/src/main/java/jp/satomaru/java8study/mastermind/MasterMind.java
+++ java8study.mastermind/src/main/java/jp/satomaru/java8study/mastermind/MasterMind.java
@@ -12,22 +12,25 @@ import javafx.stage.Stage;
 import jp.satomaru.java8study.mastermind.core.ButtonPanel;
 import jp.satomaru.java8study.mastermind.core.NumberPanel;
 import jp.satomaru.java8study.mastermind.core.NumberPanel.NumberAdapter;
+import jp.satomaru.java8study.util.IntegerMatrix;
+import jp.satomaru.java8study.util.Lottery;
 import jp.satomaru.java8study.util.Matrix;
+import jp.satomaru.java8study.util.Matrix.Item;
 
 /**
  * ヒントを元に、3x3マスの数字を当てるゲームです。
- * 
+ *
  * <p>
  * 数字マトリクスを全て数字で埋めて決定ボタンを押下すると、
  * 数字マトリクスの各列・各行に、「H:ヒット」「B:ブロー」というヒントが表示されます。
  * ヒットは「アタリ」という意味です。ブローは「ハズレだけど、その列（または行）の違うマスに数字を移動するとアタリになる」という意味です。
  * </p>
- * 
+ *
  * <p>
  * 数字を移動する時は、移動元および移動先のマスを右クリックして、マスの数字を消します。
  * その後、マスを左クリックすると、現在使用されていない数字が、クリック毎に順番に表示されます。
  * </p>
- * 
+ *
  * <p>
  * 降参する時は降参ボタン、最初からやり直す時はリセットボタンを押下してください。
  * </p>
@@ -45,7 +48,7 @@ public class MasterMind extends Application {
 
 	/**
 	 * メイン関数。
-	 * 
+	 *
 	 * @param args 使用しません
 	 */
 	public static void main(String... args) {
@@ -59,21 +62,21 @@ public class MasterMind extends Application {
 	private ButtonPanel buttonPanel;
 
 	/** 正解。 */
-	private Matrix<Integer> correctMatrix = new Matrix<Integer>(MATRIX_WIDTH, MATRIX_HEIGHT);
+	private Matrix<Integer> correctMatrix = new IntegerMatrix(MATRIX_WIDTH, MATRIX_HEIGHT);
 
 	/** 解答。 */
-	private Matrix<Integer> answerMatrix = new Matrix<Integer>(MATRIX_WIDTH, MATRIX_HEIGHT);
+	private Matrix<Integer> answerMatrix = new IntegerMatrix(MATRIX_WIDTH, MATRIX_HEIGHT);
 
 	/** 現在使用されていない数字。 */
 	private TreeSet<Integer> remains = new TreeSet<Integer>();
 
 	/**
 	 * JavaFXアプリケーションを起動します。
-	 * 
+	 *
 	 * <p>
 	 * 数字マトリクスのパネル、およびボタン群のパネルを生成し、プライマリ・ステージに表示します。
 	 * </p>
-	 * 
+	 *
 	 * @param stage プライマリ・ステージ
 	 * @throws Exception JavaFXアプリケーションの起動に失敗した場合
 	 */
@@ -98,7 +101,7 @@ public class MasterMind extends Application {
 
 	/**
 	 * アラート・ダイアログを表示します。
-	 * 
+	 *
 	 * @param type アラートタイプ
 	 * @param headerText ヘッダーメッセージ（概要メッセージ）
 	 * @param contentText 内容メッセージ（詳細メッセージ）
@@ -113,7 +116,7 @@ public class MasterMind extends Application {
 
 	/**
 	 * 決定ボタン押下時の処理を行います。
-	 * 
+	 *
 	 * <ol>
 	 * <li>解答が不完全である場合は、「空欄のままの数字があります。」「全ての数字を設定してください。」という警告をアラート・ダイアログに表示して、処理を中断します。</li>
 	 * <li>解答と正解を比較して各ヒットおよび各ブローを割り出し、数字マトリクスのパネルに表示します。</LI>
@@ -121,40 +124,87 @@ public class MasterMind extends Application {
 	 * </ol>
 	 */
 	private void onDecide() {
-		// TODO 実装してください。
+
+		if (answerMatrix.flat().anyMatch(Item::isNull)) {
+			// 解答不完全
+			alert(AlertType.WARNING, "空欄のままの数字があります。", "全ての数字を設定してください。");
+			return;
+		}
+
+		// TODO 解答と正解の比較、ヒットブローの割り出し、GUI表示
+		// 考え中です。お待ちください。
+
+		if (true) {
+			// 全問正解
+			alert(AlertType.INFORMATION, "おめでとうございます。", "全問正解です。");
+			numberPanel.setNumberDisable(true);
+			buttonPanel.setDecideDisable(true);
+			buttonPanel.setGiveupDisable(true);
+		}
 	}
 
 	/**
 	 * 降参ボタン押下時の処理を行います。
-	 * 
+	 *
 	 * <ol>
 	 * <li>数字マトリクスに正解を表示して、非活性にします。</li>
 	 * <li>数字マトリクス、決定ボタン、降参ボタンを非活性にします。</li>
 	 * </ol>
 	 */
 	private void onGiveup() {
-		// TODO 実装してください。
+
+		// 正解マトリクスをGUIに表示
+		correctMatrix.flat().forEach(item -> numberPanel.getNumberNode(item).getValue().setNumber(item.getValue()));
+
+		// 数字マトリクス、決定ボタン、降参ボタンを非活性に変更
+		numberPanel.setNumberDisable(true);
+		buttonPanel.setDecideDisable(true);
+		buttonPanel.setGiveupDisable(true);
 	}
 
 	/**
 	 * リセットボタン押下時の処理を行います。
-	 * 
+	 *
 	 * <ol>
 	 * <li>正解をランダムで生成します。</li>
-	 * <li>解答に、Z順番に数字を埋めます。</li>
+	 * <li>解答に、順番に数字を埋めます。</li>
 	 * <li>数字マトリクスに解答を表示して、活性にします。</li>
-	 * <li>全てのヒット表示およびブロー表示を空欄にします。</li>
+	 * <li>全てのヒット表示およびブロー表示を空欄します。</li>
 	 * <li>決定ボタン、降参ボタンを活性にします。</li>
 	 * <li>現在使用されていない数字をリセットします。</li>
 	 * </ol>
 	 */
 	private void onReset() {
-		// TODO 実装してください。
+
+		// 正解用数値を生成
+		Lottery<Integer> correctInitLottery = Lottery.ofInteger(1, MATRIX_WIDTH * MATRIX_HEIGHT);
+
+		// 正解マトリックスを作成、現在使用されていない数字のリセット
+		correctMatrix.flat().forEach(item -> item.setValue(correctInitLottery.draw().get()));
+
+		// 解答用数値を生成
+		Lottery<Integer> answerInitLottery = Lottery.ofInteger(1, MATRIX_WIDTH * MATRIX_HEIGHT);
+
+		// 解答に順番に数字を埋める、GUI表示
+		answerMatrix.flat()
+			.map(item -> item.setValue(answerInitLottery.draw().get()))
+			.forEach(item -> numberPanel.getNumberNode(item).getValue().setNumber(item.getValue()));
+
+		// 現在使用されていない数字をリセット
+		remains.clear();
+
+		// 全てのヒット表示およびブロー表示を空欄に変更
+		numberPanel.clearAllHitsAndBlows();
+
+		// 数字マトリックス、決定ボタン、降参ボタンを活性化
+		numberPanel.setNumberDisable(false);
+		buttonPanel.setDecideDisable(false);
+		buttonPanel.setGiveupDisable(false);
 	}
 
 	/**
 	 * 数字マトリクスが左クリックされた時の処理を行います。
-	 * 
+	 *
 	 * <ol>
 	 * <li>現在使用されていない数字が存在しない場合は、処理を中断します。</li>
 	 * <li>現在使用されていない数字の中から、クリックされた数字マスの解答よりも大きく、かつ最も近い数字を、解答に設定します。
@@ -164,26 +214,82 @@ public class MasterMind extends Application {
 	 * <li>現在使用されていない数字から、解答に設定した数字を削除します。</li>
 	 * <li>数字マスに新しい解答を表示します。</li>
 	 * </ol>
-	 * 
+	 *
 	 * @param item クリックされた数字マス
 	 */
 	private void onNumberLeftClick(Matrix.Item<NumberAdapter> item) {
-		// TODO 実装してください。
+
+		if (remains.isEmpty()) {
+			// remainsが空の時は何もしない
+			return;
+		}
+
+		// 現在設定されている値を取得
+		Item<Integer> currentAnswer = answerMatrix.get(item);
+
+		// 選択数字マスの次の値を取得
+		Integer nextInteger = getNextRemains(currentAnswer.getValue());
+
+		// 現在設定されている解答値・GUI表示を更新
+		currentAnswer.setValue(nextInteger);
+		item.getValue().setNumber(nextInteger);
 	}
 
 	/**
+	 * remainsから次の値を取得します。
+	 *	valueがnullだったらfirstを返します。
+	 *	valueがnullでなかったらhigherを返します。
+	 *	higherが取得できなければfirstを返します。
+	 *	現在使用していない数字の整合性を保ちます。
+	 * @param value
+	 * @return
+	 */
+	private Integer getNextRemains(Integer value) {
+
+		if (value == null) {
+			// firstを取得し、未使用数字から除去
+			Integer first = remains.first();
+			remains.remove(first);
+			return first;
+		}
+
+		// 次の値を取得
+		Integer next = remains.higher(value);
+		if (next == null) {
+			next = remains.first();
+		}
+
+		// 使用されていない数字の入れ替え
+		remains.add(value);
+		remains.remove(next);
+
+		return next;
+	};
+
+	/**
 	 * 数字マトリクスが右クリックされた時の処理を行います。
-	 * 
+	 *
 	 * <ol>
 	 * <li>クリックされた数字マスの解答が設定されていない場合は、処理を中断します。</li>
 	 * <li>クリックされた数字マスの表示を空欄にします。</li>
 	 * <li>現在使用されていない数字に、クリックされた数字マスの解答を追加します。</li>
 	 * <li>クリックされた数字マスの解答を消去します。</li>
 	 * </ol>
-	 * 
+	 *
 	 * @param item クリックされた数字マス
 	 */
 	private void onNumberRightClick(Matrix.Item<NumberAdapter> item) {
-		// TODO 実装してください。
+
+		Item<Integer> selectedItem = answerMatrix.get(item);
+
+		if (!selectedItem.isNull()) {
+
+			// 現在値を未使用数字へ
+			remains.add(selectedItem.getValue());
+
+			// 正解マトリックスとGUIをクリア
+			selectedItem.clear();
+			item.getValue().setNumber(null);
+		}
 	}
 }
