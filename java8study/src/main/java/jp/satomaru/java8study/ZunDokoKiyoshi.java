ee9011a67f7bdf0a0fa13993e927646ec09b61bc
Satomaru <erynenadan@gmail.com>
2016-03-11 16:24:51 +0900

    ズンドコキ・ヨ・シ！を作成しました。

---
+++ java8study/src/main/java/jp/satomaru/java8study/ZunDokoKiyoshi.java
@@ -0,0 +1,45 @@
+package jp.satomaru.java8study;
+
+import java.util.function.Consumer;
+
+import lombok.RequiredArgsConstructor;
+
+public class ZunDokoKiyoshi {
+
+	@RequiredArgsConstructor
+	public enum Word {
+		ZUN("ズン"), DOKO("ドコ"), KIYOSHI("キ・ヨ・シ！");
+		private final String string;
+
+		@Override
+		public String toString() {
+			return string;
+		}
+	}
+
+	public static final int ZUN_CHAIN_WHEN_FINISH = 4;
+
+	public static void main(String[] args) {
+		execute(System.out::print);
+	}
+
+	public static void execute(Consumer<Word> acceptor) {
+		int zunChain = 0;
+		Word word = null;
+		boolean finish = false;
+
+		do {
+			word = (Math.random() > 0.5) ? Word.ZUN : Word.DOKO;
+			acceptor.accept(word);
+
+			if (word.equals(Word.ZUN)) {
+				++zunChain;
+			} else if (zunChain >= ZUN_CHAIN_WHEN_FINISH) {
+				acceptor.accept(Word.KIYOSHI);
+				finish = true;
+			} else {
+				zunChain = 0;
+			}
+		} while (!finish);
+	}
+}
